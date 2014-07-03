7a31a289cc0081728a9766bef1c76389a4bd34da
cinjon <cinjon.resnick@gmail.com>
2014-07-03 11:46:13 -0700

    app: gitignore, config, reqs

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+CSRF_ENABLED = True
+SECRET_KEY = os.environ.get('POETREE_SECRET_KEY')
+basedir = os.path.abspath(os.path.dirname(__file__))
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = os.environ.get('POETREE_DATABASE_URL', None)
