16c70e5ebade60a8d94a8a12356c35df685822d8
cinjon <cinjon.resnick@gmail.com>
2014-07-07 16:05:34 -0700

    models: wit entity info and format_to_css

--- app/models.py
+++ app/models.py
@@ -6,13 +6,18 @@ class Poet(db.Model):
     name = db.Column(db.Text())
     poems = db.relationship('Poem', lazy='dynamic', backref='poet')
     creation_time = db.Column(db.DateTime)
+    pushed_to_wit = db.Column(db.Boolean)
 
     def __init__(self, name):
         self.name = name
         self.creation_time = utility.get_time()
+        self.pushed_to_wit = False
 
     def get_name(self):
         return self.name.title()
+
+    def wit_entity_id(self):
+        return 'poet_name'
 
     def display_poems(self):
         return [poem.display_self() for poem in self.poems.all()]
@@ -32,6 +37,7 @@ def get_or_create_poet(name):
 class Poem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     creation_time = db.Column(db.DateTime)
+    pushed_to_wit = db.Column(db.Boolean)
     title = db.Column(db.Text())
     text = db.Column(db.Text())
     youtube = db.Column(db.Text()) # youtube url
@@ -44,6 +50,7 @@ class Poem(db.Model):
         self.youtube = youtube
         self.audio = audio
         self.creation_time = utility.get_time()
+        self.pushed_to_wit = False
 
     def set_youtube(self, youtube):
         self.youtube = youtube
@@ -52,8 +59,11 @@ class Poem(db.Model):
     def get_title(self):
         return self.title.title()
 
+    def wit_entity_id(self):
+        return 'poem_name'
+
     def display_self(self):
-        return {'text':self.text, 'title':self.get_title(), 'youtube':self.youtube, 'audio':self.audio, 'poet':Poet.query.get(self.poet_id).get_name()}
+        return {'text':format_to_css(self.text), 'title':self.get_title(), 'youtube':self.youtube, 'audio':self.audio, 'poet':Poet.query.get(self.poet_id).get_name()}
 
 def create_poem(title, text, youtube, audio, poet_id):
     poet = Poet.query.get(poet_id)
@@ -65,3 +75,15 @@ def create_poem(title, text, youtube, audio, poet_id):
     db.session.add(poem)
     db.session.commit()
     return poem
+
+def format_to_css(text):
+    parts = [p for p in text.strip('\n').split('\n')]
+    for num,p in enumerate(parts):
+        if p == '':
+            continue
+        k = 0
+        while p[k] == ' ':
+            k += 1
+            if k > 0:
+                parts[num] = p.replace(p[:k], '&nbsp;'*k)
+    return '<p>' + '</p><p>'.join(parts) + '</p>'
