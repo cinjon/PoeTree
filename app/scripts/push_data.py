90e2879860ae11479b16289e0a635fade7bd8cc7
cinjon <cinjon.resnick@gmail.com>
2014-07-11 11:17:53 -0700

    push_data: adj

--- app/scripts/push_data.py
+++ app/scripts/push_data.py
@@ -4,35 +4,31 @@ from wit import Wit
 def get_wit():
     return Wit(app.wit_access_token)
 
-def push_poets(wit=None, create_new=False):
+def push_poets(wit=None):
     wit = wit or get_wit()
     entity_values = [
         {'value':poet.name, 'expressions':[poet.name, poet.name.split(' ')[-1]]} for poet in app.models.Poet.query.filter(app.models.Poet.pushed_to_wit == False).all()]
-    if create_new:
-        wit.post_entity('poet_name', values=entity_values)
-    else:
-        wit.update_entity('poet_name', values=entity_values)
+    wit.update_entity('poet', values=entity_values)
+
     for poet in app.models.Poet.query.filter(app.models.Poet.pushed_to_wit == False).all():
         poet.pushed_to_wit = True
     app.db.session.commit()
 
-def push_poems(wit=None, create_new=False):
+def push_poems(wit=None):
     wit = wit or get_wit()
     entity_values = [
         {'value':poem.title, 'expressions':[poem.title]} for poem in app.models.Poem.query.filter(app.models.Poem.pushed_to_wit == False).all()]
-    if create_new:
-        wit.post_entity('poem_name', values=entity_values)
-    else:
-        wit.update_entity('poem_name', values=entity_values)
+    wit.update_entity('poem', values=entity_values)
     for poem in app.models.Poem.query.filter(app.models.Poem.pushed_to_wit == False).all():
         poem.pushed_to_wit = True
     app.db.session.commit()
 
-def push_all(create_new=False):
+def push_all():
     print "Starting push to Wit"
     wit = get_wit()
-    print "Pushing Poets"
-    push_poets(wit, create_new)
-    print "Done pushing poets. Pushing Poems"
-    push_poems(wit, create_new)
+    print "Pushing Poets:"
+    push_poets(wit)
+    print "Done pushing poets."
+    print "Pushing Poems:"
+    push_poems(wit)
     print "Done pushing poems"
