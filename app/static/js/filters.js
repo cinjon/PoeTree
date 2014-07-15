dd47b38ba7c20c28624ab1baa0ec7a9f9fb11744
cinjon <cinjon.resnick@gmail.com>
2014-07-15 12:41:23 -0700

    client side: small adj to css (width of Choose ###) and filters (numToStr)

--- app/static/js/filters.js
+++ app/static/js/filters.js
@@ -25,7 +25,8 @@ angular.module('poetreeFilters', [])
     }
   })
   .filter('numToStr', function() {
-    dict = {1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five'}
+    dict = {1:'One', 2:'Two', 3:'Three', 4:'Four', 5:'Five',
+            6:'Six', 7:'Seven', 8:'Eight', 9:'Nine', 10:'Ten'}
     return function(input) {
       if (input in dict) {
         return dict[input];
