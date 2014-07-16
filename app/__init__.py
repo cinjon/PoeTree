b87c80419852d5164f78a20e34c07ae6a418403c
cinjon <cinjon.resnick@gmail.com>
2014-07-16 11:46:58 -0400

    remove before request db create - unnecessary

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,6 @@ flask_app = Flask(__name__, template_folder='public/template')
 flask_app.config.from_object('config')
 db = SQLAlchemy(flask_app)
 
-@flask_app.before_first_request
-def before_first_request():
-    try:
-        db.create_all()
-    except Exception, e:
-        flask_app.logger.error(str(e))
-
 import models
 import controllers
 import scripts
