c45b7b6d6e19c7635cdffaec741a5bebdea8cf9e
cinjon <cinjon.resnick@gmail.com>
2014-07-17 12:03:25 -0400

    hosting: updated.

--- run.py
+++
@@ -1,3 +0,0 @@
-import app
-if __name__ == "__main__":
-    app.flask_app.run(host='0.0.0.0', port=8080)
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from app import flask_app
+
+if __name__ == "__main__":
+    print 'in main...'
+    flask_app.run()
