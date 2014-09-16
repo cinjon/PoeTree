23f95ff819b646d7b20de2c7a6b015200c3c42cb
cinjon <cinjon.resnick@gmail.com>
2014-09-16 12:05:20 -0400

    blur search and add ipad to mobile

--- app/static/js/directives.js
+++ app/static/js/directives.js
@@ -3,3 +3,19 @@
 /* Directives */
 
 angular.module('poetreeDirectives', [])
+  .directive('blurMe', function($timeout) {
+    return {
+      scope: { trigger: '@blurMe' },
+      link: function(scope, element) {
+        scope.$watch('trigger', function(value) {
+          console.log(element);
+          if(value === "true") {
+            $timeout(function() {
+              console.log('blurring');
+              element[0].blur();
+            });
+          }
+        });
+      }
+    };
+  });
